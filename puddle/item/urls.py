fc568f5e8405d014e68d9de9bb190c456f2e6012
Kasse-Dembele <kasse.dembele@gmail.com>
2023-02-08 18:08:45 +0100

    feat(detail): i have now the route for item detail

---
+++ puddle/item/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import detail
+
+app_name = "item"
+
+urlpatterns=[
+    path("<int:item_id>/", detail, name="detail" )
+]
--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Item
+
+def detail(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    same_category_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=item_id)[0:3]
+    return render(request, "item/detail.html", {
+        "item": item,
+        "same_category_items": same_category_items
+    })
+    
