6a43f3f830b9192ed7264488d1d02073268d7dd3
Kasse-Dembele <kasse.dembele@gmail.com>
2023-02-09 17:05:28 +0100

    style(item): add css class to add new item form

--- puddle/item/form.py
+++ puddle/item/form.py
@@ -1,9 +1,28 @@
 from django import forms
 from .models import Item
+INPUTS_CLASS = "py-4 px-6 w-full rounded-xl border"
 
 class AddItemForm(forms.ModelForm):
+
     class Meta:
         model = Item
         fields = ("category", "name", "description", "price", "image")
+        widgets = {
+            'category': forms.Select(attrs={
+            'class': INPUTS_CLASS
+            }),
+            'name': forms.TextInput(attrs={
+            'class': INPUTS_CLASS
+            }),
+            'description': forms.Textarea(attrs={
+            'class': INPUTS_CLASS
+            }),
+            'price': forms.TextInput(attrs={
+            'class': INPUTS_CLASS
+            }),
+            'image': forms.FileInput(attrs={
+            'class': INPUTS_CLASS
+            })
+        }
 
 
