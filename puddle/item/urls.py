ac246ac33655d75184a5461ad5e72c5223774f68
Kasse-Dembele <kasse.dembele@gmail.com>
2023-02-09 17:03:04 +0100

    feat(item): as a user a can add new item througt a form

--- puddle/item/urls.py
+++ puddle/item/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import detail
+from .views import detail, addNewItem
 
 app_name = "item"
 
 urlpatterns=[
-    path("<int:item_id>/", detail, name="detail" )
+    path("<int:item_id>/", detail, name="detail" ),
+    path("new/",addNewItem, name="new_item" )
 ]
--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .form import AddItemForm
 # Create your views here.
 from .models import Item
 
@@ -10,4 +11,20 @@ def detail(request, item_id):
         "item": item,
         "same_category_items": same_category_items
     })
+
+@login_required
+def addNewItem(request):
+    if request.method == "POST":
+        form = AddItemForm(request.POST, request.FILES)
+        item = form.save(commit=False)
+        item.created_by = request.user
+        item.save()
+        return redirect("item:detail", item_id=item.id)
+
+    else:
+        form = AddItemForm()
+
+        return render(request, "item/form.html", {
+            'form': form
+        })
     
