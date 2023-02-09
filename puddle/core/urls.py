a783904cbab1de51ccc083775a59202b19097f13
Kasse-Dembele <kasse.dembele@gmail.com>
2023-02-09 14:24:50 +0100

    feat(core): as a user i have a login route

--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+from . import forms
 
 
 app_name = "core"
@@ -7,6 +9,8 @@ app_name = "core"
 urlpatterns = [
     path("", views.index, name="index"),
     path("contact/",views.contact, name="contact" ),
-    path("signup/", views.signup, name="signup")
+    path("signup/", views.signup, name="signup"),
+    path("login/", auth_views.LoginView.as_view(authentication_form=forms.LoginForm, template_name="core/login.html"), name="login")
+
     
 ]
